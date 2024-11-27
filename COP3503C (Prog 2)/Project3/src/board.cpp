e94e70362498d9242eb0819ebf279acc9d7db4f7
Mandar Ghade <GX2H912@gmail.com>
2024-11-26 21:47:11 -0500

    chore: `COP3503C`: Minesweeper (organized `Board` & `Tile`)

--- COP3503C (Prog 2)/Project3/src/board.cpp
+++ COP3503C (Prog 2)/Project3/src/board.cpp
@@ -1,2 +1,90 @@
 #include "board.h"
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <random>
+#include "config.h"
+#include "tile.h"
+
+
+void Board::generate_board() {
+	for (size_t i = 0; i < this->rows; i++) {
+		tiles.push_back(std::vector<Tile*>());
+		for (size_t j = 0; j < this->columns; j++) {
+			tiles.at(i).push_back(
+				new Tile((int)j, (int)i, (int)this->rows, this->cfg)
+			);
+		}
+	}
+	this->assign_neighboring_tiles();
+	this->plant_mines();
+}
+
+void Board::assign_neighboring_tiles() {
+	for (auto row = tiles.begin(); row != tiles.end(); row++) {
+		for (auto col = row->begin(); col != row->end(); col++) {
+			(*col)->assign_neighboring_tiles(tiles);
+		}
+	}
+}
+
+void Board::plant_mines() {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> row_dist(0, static_cast<int>(this->rows) - 1);
+	std::uniform_int_distribution<> col_dist(0, static_cast<int>(this->columns) - 1);
+	for (size_t i = 0; i < this->mine_count; i++) {
+		int row = row_dist(gen);
+		int col = col_dist(gen);
+		while (this->tiles[row][col]->has_mine()) {
+			row = row_dist(gen);
+			col = col_dist(gen);
+		}
+		this->tiles[row][col]->plant_mine();
+		this->mines.push_back(this->tiles[row][col]);
+		this->tiles[row][col]->incr_adj_mine_counts();
+	}
+}
+
+void Board::reset() {
+	this->mines.clear();
+	for (auto row = tiles.begin(); row != tiles.end(); row++) {
+		for (auto col = row->begin(); col != row->end(); col++) {
+			delete (*col);
+		}
+	}
+	this->tiles.clear();
+	generate_board();
+}
+void Board::draw_sprites(sf::RenderWindow& window) {
+	for (auto row = tiles.begin(); row != tiles.end(); row++) {
+		for (auto col = row->begin(); col != row->end(); col++) {
+			window.draw((*col)->get_sprite());
+		}
+	}
+}
+
+void Board::print_as_str() {
+	std::cout << "Board {" << '\n';
+	std::cout << "\trows = " << this->rows << ',' << '\n';
+	std::cout << "\tcolumns = " << this->columns << ',' << '\n';
+	std::cout << "\tmines = " << this->mine_count << ',' << '\n';
+	std::cout << "\tboard = " << "{" << '\n';
+	for (auto row = tiles.begin(); row != tiles.end(); row++) {
+		for (auto col = row->begin(); col != row->end(); col++) {
+			std::cout << "\t\t" << (*col)->as_str() << ',' << '\n';
+			std::cout << "\t\t\tneighbors = {" << '\n';
+			Tile::AdjacentTiles neighbors = (*col)->get_neighbors();
+			for (size_t i = 0; i < 8; i++) {
+				if (neighbors.adjacent[i] != nullptr) {
+					std::cout << "\t\t\t\t" << neighbors.adjacent[i]->as_str() << '\n';
+				}
+			}
+			std::cout << "\t\t\t}" << '\n';
+		}
+	}
+	std::cout << "\t}\n"; 
+	std::cout << "}\n";
+}
 
--- COP3503C (Prog 2)/Project3/src/tile.h
+++ COP3503C (Prog 2)/Project3/src/tile.h
@@ -45,142 +45,21 @@ public:
 		);
 	};
 
-	sf::Sprite& get_sprite() {
-		return this->sprite;
-	}
-
-	std::string as_str() const {
-		std::string has_a_mine = this->has_mine() ? "true" : "false";
-		std::string output_msg = "Tile { x = " 
-			+ std::to_string(this->x) + ", y = " 
-			+ std::to_string(y) + ", has_mine = " + has_a_mine + ", adjacent_mines = " 
-			+ std::to_string(this->adjacent_mines) + " }";
-		return output_msg;
-	}
-
-	void plant_mine() {
-		this->mine = new Mine(this->x, this->y);
-		this->sprite.setTexture(this->cfg.textures.mine);
-	}
-
-	void set_matching_texture() {
-		if (this->has_mine()) {
-			this->sprite.setTexture(this->cfg.textures.mine);
-			return;
-		}
-		if (this->adjacent_mines == 1) {
-			this->sprite.setTexture(this->cfg.textures.number_1);
-		} else if (this->adjacent_mines == 2) {
-			this->sprite.setTexture(this->cfg.textures.number_2);
-		} else if (this->adjacent_mines == 3) {
-			this->sprite.setTexture(this->cfg.textures.number_3);
-		} else if (this->adjacent_mines == 4) {
-			this->sprite.setTexture(this->cfg.textures.number_4);
-		} else if (this->adjacent_mines == 5) {
-			this->sprite.setTexture(this->cfg.textures.number_5);
-		} else if (this->adjacent_mines == 6) {
-			this->sprite.setTexture(this->cfg.textures.number_6);
-		} else if (this->adjacent_mines == 7) {
-			this->sprite.setTexture(this->cfg.textures.number_7);
-		} else if (this->adjacent_mines == 8) {
-			this->sprite.setTexture(this->cfg.textures.number_8);
-		} 
-	}
-
-	void incr_adj_mine_counts() const {
-		// assumes current tile has a mine.
-
-		if (!this->has_mine()) {
-			throw std::runtime_error("Cannot increment adjacent mine counts if starting Tile is not a mine!");
-		}
-
-		for (size_t i = 0; i < 8; i++) {
-			if (this->tiles.adjacent[i] != nullptr) {
-				if (!this->tiles.adjacent[i]->has_mine()) {
-					this->tiles.adjacent[i]->adjacent_mines++;
-					this->tiles.adjacent[i]->set_matching_texture();
-				}
-			}
-		}
-	}
-
-	void reveal_tile() {
-		// use this when revealing tiles without mines
-		if (this->has_mine()) {
-			throw std::runtime_error("Attempted to reveal a mine-containing tile. Use detonate() instead.");
-		}
-		this->current_state = TILE_STATE::REVEALED;
-	}
-
-	bool has_mine() const {
-		return mine != nullptr;
-	};
-
-	bool has_detonated() const {
-		if (!this->has_mine()) {
-			throw std::runtime_error("Mine does not exist on this Tile. Cannot detonate.");
-		}
-		return this->mine->has_detonated;
-	}
-
-	TILE_STATE get_state() const {
-		return this->current_state;
-	}
-
-	int get_adjacent_mines() const {
-		return this->adjacent_mines;
-	}
-
-	bool get_is_flagged() const {
-		return this->flagged;
-	}
-
-	void set_flag_state(bool is_flagged) {
-		if (this->current_state == TILE_STATE::REVEALED) {
-			throw std::runtime_error("Cannot flag a tile that has already been revealed!");
-		}
-		this->flagged = is_flagged;
-	}
-
-	void detonate_mine() {
-		if (!this->has_mine()) {
-			throw std::runtime_error("Mine does not exist! Cannot detonate");
-		}
-		this->mine->has_detonated = true;
-		this->current_state = TILE_STATE::REVEALED;
-	}
-
-	void assign_neighboring_tiles(
-		const std::vector<std::vector<Tile*>>& board
-	) {
-		size_t max_rows = board.size();
-		if (max_rows == 0) {
-			throw std::runtime_error("Board uninitialized! Cannot compute neighboring tiles.");
-		}
-		size_t max_cols = board.at(0).size();
-		size_t row = this->y;
-		size_t col = this->x;
-		size_t col_start, col_end;
-		size_t row_start, row_end;
-		col_start = std::max(static_cast<int>(col) - 1, 0);
-		col_end = std::min(static_cast<int>(col) + 2, static_cast<int>(max_cols)); // for loop bounds
-		row_start = std::max(static_cast<int>(row) - 1, 0);
-		row_end = std::min(static_cast<int>(row) + 2, static_cast<int>(max_rows));
-		size_t count = 0;
-		for (size_t r = row_start; r < row_end; r++) {
-			for (size_t c = col_start ; c < col_end; c++) {
-				if (c == col && r == row) {
-					continue;
-				}
-				this->tiles.adjacent[count++] = board.at(r).at(c);
-			}
-		}
-	}
-
-	AdjacentTiles get_neighbors() const {
-		return this->tiles;
-	}
-
+	sf::Sprite& get_sprite(); 
+	std::string as_str() const; 
+	void plant_mine(); 
+	void set_matching_texture(); 
+	void incr_adj_mine_counts() const; 
+	void reveal_tile(); 
+	bool has_mine() const;
+	bool has_detonated() const; 
+	TILE_STATE get_state() const; 
+	int get_adjacent_mines() const; 
+	bool get_is_flagged() const; 
+	void set_flag_state(bool is_flagged); 
+	void detonate_mine(); 
+	void assign_neighboring_tiles(const std::vector<std::vector<Tile*>>& board); 
+	AdjacentTiles get_neighbors() const; 
 	~Tile() {
 		if (this->mine != nullptr) {
 			delete mine;
